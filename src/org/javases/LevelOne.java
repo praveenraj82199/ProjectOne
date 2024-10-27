ca430546b1bb667e6f7f3c6fdf45df148e6efa39
praveenraj82199 <rajpraveen648@gmail.com>
2024-10-27 07:33:06 +0530

    new two

--- src/org/javases/LevelOne.java
+++ src/org/javases/LevelOne.java
@@ -3,6 +3,8 @@ package org.javases;
 public class LevelOne {
 	public static void main(String[] args) {
 		System.out.println("FIRST PUSHING");
+		
+		System.out.println("SECOUNT PUSHING");
 	}
 
 }
